96cd54a2edc8685addefdd0a4e48a32afbda0720
Kate Summerill <kaitlynsummerill@gmail.com>
2019-10-24 16:45:55 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,9 @@
 # code your #position_taken? method here!
 
 def position_taken?(array, index)
-  if array[index] == " " || "" || nil
+  if array[index] == " " || array[index] == "" || array[index] == nil
     return false
-  elsif array[index] == "X" || "O"
+  elsif array[index] == "X" || array[index] == "O"
     return true
   end
 end
